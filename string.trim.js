df87452499b25a665ad40b62e07d0f63dd0b01e1
yanhechao <yanhechao@outlook.com>
2016-08-16 19:38:06 +0800

    Update string.trim.js

--- string.trim.js
+++ string.trim.js
@@ -1,7 +1,5 @@
 /*
-	如果浏览器本身不支持String对象的trim方法,
-	那么运行下面的代码可以兼容这些环境.
-	@see https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+@see https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
 */
 if(!String.prototype.trim) {
   String.prototype.trim = function () {
